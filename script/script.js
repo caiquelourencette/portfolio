cfccf5d1355c281078e7d34ad670cda998e2c487
Caique Fernando <caique_flr@hotmail.com>
2023-06-06 00:32:20 -0300

    add alterações

--- script/script.js
+++ script/script.js
@@ -8,3 +8,13 @@ window.onscroll = function () {
     header.classList.remove('navbarDark')
   }
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    })
+  })
+})
